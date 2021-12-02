ea61d901f43dd21651ddb14083504b1b08bcc762
mwait <mwaitzman@outlook.com>
2021-12-01 20:40:51 -0500

    add solution for d2p2

---
+++ day2_part2/src/main.rs
@@ -0,0 +1,35 @@
+use std::fs::File;
+use std::io::Read;
+use std::str;
+use std::str::FromStr;
+
+fn main() {
+    let mut file = File::open("./input").unwrap();
+    let mut data = Vec::new();
+    file.read_to_end(&mut data).unwrap();
+    let mut lines = vec![];
+    let mut j = 0;
+    for (i, byte) in data.iter().enumerate() {
+        if let b'\n' = byte {
+            lines.push(str::from_utf8(&data[j..i]).unwrap());
+            j = i + 1;
+        }
+    }
+    let mut x = 0;
+    let mut y = 0;
+    let mut aim = 0;
+    lines.iter().for_each(|line| {
+        if let Some(_) = line.find("forward") {
+            x += i32::from_str(line.split_whitespace().nth(1).unwrap()).unwrap();
+            y += aim * i32::from_str(line.split_whitespace().nth(1).unwrap()).unwrap();
+        }
+        else if let Some(_) = line.find("up") {
+            aim -= i32::from_str(line.split_whitespace().nth(1).unwrap()).unwrap();
+        }
+        else if let Some(_) = line.find("down") {
+            aim += i32::from_str(line.split_whitespace().nth(1).unwrap()).unwrap();
+        }
+        else {panic!()};
+    });
+    println!("answer: {}", x * y);
+}
